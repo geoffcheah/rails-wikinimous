3e2d3fd31f8aaae8bfcb158f7733aa8cec702699
Geoffrey Cheah <geoffcheah@gmail.com>
2018-02-20 18:46:53 +0000

    finished creating the destroy method in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,15 +17,19 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-
+    @article = Article.find(params[:id])
   end
 
   def update
-
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path
   end
 
   def destroy
-
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
